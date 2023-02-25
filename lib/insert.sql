fe03494f5e68a9319c56a70eaec5f66b1f7375fc
Robert Gorz <rjgorz@gmail.com>
2023-02-24 21:32:25 -0600

    completed SQL queries

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Mr. Chocolate", 14, "M", "Brown", "Sweet", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Rowdy", 23, "F", "Black", "Fiesty", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Tabitha", 8, "F", "Gray", "Positive", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Sergeant Brown", 56, "M", "Brown", "Loyal", 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Melissa", 32, "F", "Black", "Nice", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Grinch", 87, "F", "Green", "Angry", 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Wendy", 15, "F", "Red", "Hungry", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (NULL, 1, "M", "Gray", "Sleepy", 1);
